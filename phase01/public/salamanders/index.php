dff2d3860a1cb5116a9af5a0b1aff42e7d876348
leonardoamichael <leonardoamichael@students.abtech.edu>
2025-02-25 06:02:08 -0500

    Code for phase01 is ready

---
+++ phase01/public/salamanders/index.php
@@ -0,0 +1,56 @@
+<!-- require initialize.php -->
+<?php require_once('../../private/initialize.php'); ?>
+
+
+<!-- 
+  Write a salamanders array with the following
+id=1, salamanderName = Red-Legged Salamander
+id=2, salamanderName = Pigeon Mountain Salamander
+id=3', salamanderName = ZigZag Salamander
+id=4,  salamanderName= Slimy Salamander 
+-->
+
+<?php
+$salamanders = [
+  ['id' => 1, 'salamanderName' => 'Red-Legged Salamander'],
+  ['id' => 2, 'salamanderName' => 'Pigeon Mountain Salamander'],
+  ['id' => 3, 'salamanderName' => 'ZigZag Salamander'],
+  ['id' => 4, 'salamanderName' => 'Slimy Salamander']
+];
+?>
+
+
+
+<!-- Add the pageTitle for salamanders
+Include a shared path to the salamander header -->
+<?php 
+$pageTitle = 'Salamanders';
+include(SHARED_PATH . '/salamander-header.php');
+?>
+
+<h1>Salamanders</h1>
+
+  <a href="#">Create Salamander</a>
+
+<table>
+  <tr>
+    <th>ID</th>
+    <th>Name</th>
+    <th>&nbsp;</th>
+    <th>&nbsp;</th>
+    <th>&nbsp;</th>
+  </tr>
+
+  <?php foreach($salamanders as $salamander) { ?>
+  <tr>
+    <td><?php echo h($salamander['id']); ?></td>
+    <td><?php echo h($salamander['salamanderName']); ?></td>
+    <td><a href="<?php echo url_for('/salamanders/show.php?id=' . h(u($salamander['id']))); ?>">View</a></td>
+    <td><a href="#">Edit</a></td>
+    <td><a href="#">Delete</a></td>
+  </tr>
+<?php } ?>
+
+  	</table>
+
+<?php include(SHARED_PATH . '/salamander-footer.php');?>
